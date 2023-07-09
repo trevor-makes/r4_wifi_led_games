33be0d031d915d7a748b02f24f925a33585eb1ed
Trevor Makes <the.trevor.makes@gmail.com>
2023-07-09 00:59:54 -0700

    Replace serial with moving pixel

--- r4_wifi_snake.ino
+++ r4_wifi_snake.ino
@@ -1,26 +1,29 @@
+#include "Arduino_LED_Matrix.h"
 #include "PlayStation.h"
+
+ArduinoLEDMatrix matrix;
+
+constexpr size_t WIDTH = 12;
+constexpr size_t HEIGHT = 8;
+byte frame[HEIGHT][WIDTH];
+uint8_t row, col;
 
 void setup() {
   PlayStation.begin();
-
-  Serial.begin(9600);
-  while (!Serial) {}
+  matrix.begin();
+  memset(frame, 0, sizeof(frame));
+  row = HEIGHT / 2;
+  col = WIDTH / 2;
 }
 
 void loop() {
+  frame[row][col] = 0;
   PlayStation.update();
   uint16_t pressed = PlayStation.get_pressed();
-  if (pressed & PlayStation.Left) {
-    Serial.println("Left");
-  }
-  if (pressed & PlayStation.Right) {
-    Serial.println("Right");
-  }
-  if (pressed & PlayStation.Up) {
-    Serial.println("Up");
-  }
-  if (pressed & PlayStation.Down) {
-    Serial.println("Down");
-  }
-  delay(10);
+  if ((pressed & PlayStation.Left) && col > 0) --col;
+  if ((pressed & PlayStation.Right) && col < WIDTH - 1) ++col;
+  if ((pressed & PlayStation.Up) && row > 0) --row;
+  if ((pressed & PlayStation.Down) && row < HEIGHT - 1) ++row;
+  frame[row][col] = 1;
+  matrix.renderBitmap(frame, HEIGHT, WIDTH);
 }
