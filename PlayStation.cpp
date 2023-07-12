fe0234e46d9777b22a2caf44a82214fb1164766f
Trevor Makes <the.trevor.makes@gmail.com>
2023-07-12 04:33:33 -0700

    Remove SPI hack

--- PlayStation.cpp
+++ PlayStation.cpp
@@ -2,14 +2,6 @@
 
 PlayStation_t PlayStation;
 
-// See https://github.com/arduino/ArduinoCore-renesas/issues/12 for SPI fix
-#define SPI_HACK
-#if defined(ARDUINO_MINIMA)
-#define SPI_SPCMD R_SPI1->SPCMD_b[0]
-#elif defined(ARDUINO_UNOWIFIR4)
-#define SPI_SPCMD R_SPI0->SPCMD_b[0]
-#endif
-
 void PlayStation_t::begin(pin_size_t chip_select) {
   chip_select_ = chip_select;
   pinMode(chip_select_, OUTPUT);
@@ -19,12 +11,6 @@ void PlayStation_t::begin(pin_size_t chip_select) {
 
 void PlayStation_t::update() {
   SPI.beginTransaction(settings_);
-#ifdef SPI_HACK
-  // Hack to set LSBFIRST and SPI_MODE3
-  SPI_SPCMD.CPHA = 1;
-  SPI_SPCMD.CPOL = 1;
-  SPI_SPCMD.LSBF = 1;
-#endif
   digitalWrite(chip_select_, LOW);
 
   delayMicroseconds(50);
@@ -41,12 +27,6 @@ void PlayStation_t::update() {
 
   digitalWrite(chip_select_, HIGH);
   SPI.endTransaction();
-#ifdef SPI_HACK
-  // Hack to clear LSBFIRST and SPI_MODE3
-  SPI_SPCMD.CPHA = 0;
-  SPI_SPCMD.CPOL = 0;
-  SPI_SPCMD.LSBF = 0;
-#endif
 
   last_buttons_ = buttons_;
   buttons_ = ~((buttons2 << 8) | buttons1);
